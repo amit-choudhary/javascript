838e3e857565df92e04aff08c7137c435c3c612e
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-24 13:21:12 +0530

    Numeral Matching Exercise

---
+++ matching_numeral/matching_numeral.js
@@ -0,0 +1,31 @@
+function CheckForm(formElement, numberTextField, resultTextField) {
+this.formElement = formElement;
+this.numberTextField = numberTextField;
+this.resultTextField = resultTextField;
+}
+
+CheckForm.prototype.bindEvents = function() {
+  var flag = '';
+  var _this = this;
+  this.formElement.addEventListener('submit', function(event) {
+    flag = _this.checkNumber(_this.numberTextField.value);
+    _this.writeResult(flag);
+    flag ? event.preventDefault() : '';
+  } );  
+}
+
+CheckForm.prototype.checkNumber = function(number) {
+  return isNaN(number);
+}
+
+CheckForm.prototype.writeResult = function(flag) {
+  this.resultTextField.value = !flag;
+}
+
+window.onload = function() {
+  var formElement = document.forms[0];
+  var numberTextField = document.getElementById('number');
+  var resultTextField = document.getElementById('result');
+  var checkform = new CheckForm(formElement, numberTextField, resultTextField);
+  checkform.bindEvents();
+}
