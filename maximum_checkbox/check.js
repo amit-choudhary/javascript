31d08b6b01ee000d22cc10d8b7eb53470efe6b3d
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-24 14:24:23 +0530

    Fix functionality, fix naming issues, added eventlistener instead of onclick and optimise code

--- maximum_checkbox/check.js
+++ maximum_checkbox/check.js
@@ -1,53 +1,62 @@
-function CheckBox(days, none) {
-  this.days = days;
-  this.none = none;
+function Days(daysElement, noneCheckbox) {
+  this.daysElement = daysElement;
+  this.noneCheckbox = noneCheckbox;
   this.count = 0;
+  this.checkedDays = [];
 }
 
-CheckBox.prototype.bindEvents = function() {
+Days.prototype.bindEvents = function() {
   var _this = this;
-  for(var i = 0; i < _this.days.length; i++) {
-    _this.days[i].onclick = function() {
+  for(var i = 0; i < _this.daysElement.length; i++) {
+    _this.daysElement[i].addEventListener('click', function() {
       _this.countCheck(this);
-      _this.none.checked = false;
-    }
+      _this.noneCheckbox.checked = false;
+    } );
   }
-  _this.none.onclick = function() {
+  _this.noneCheckbox.addEventListener('click', function() {
     _this.mark(false);
     _this.count = 0;
-  }
+  } );
 }
 
-CheckBox.prototype.countCheck = function(day) {
+Days.prototype.countCheck = function(day) {
   if (day.checked == true) {
     ++this.count;
+    this.checkedDays.push(day.value);
+  }
+  else {
+    --this.count;
+    var index = this.checkedDays.indexOf(day.value);
+    this.checkedDays.splice(index, 1);
+  }
     if (this.count > 3) {
       day.checked = false;
+      index = this.checkedDays.indexOf(day.value);
+      this.checkedDays.splice(index, 1);
+      --this.count;
       alert('Only 3 days can be selected. You have already selected' + this.showSelected()); 
     }
-  }
 }
 
-CheckBox.prototype.mark = function(flag) {
-  for (var i = 0; i < this.days.length; i++) {
-    this.days[i].checked = flag;
+Days.prototype.mark = function(flag) {
+  for (var i = 0; i < this.daysElement.length; i++) {
+    this.daysElement[i].checked = flag;
   }
+  this.checkedDays = [];
 }
 
-CheckBox.prototype.showSelected = function() {
-  var str = ' '
-  console.log(this);
-  for (var j = 0;j < this.days.length; j++) {
-    if (this.days[j].checked == true) {
-      str += ' ' + this.days[j].value;
-    }
+Days.prototype.showSelected = function() {
+  var str = ' ' + this.checkedDays[0];
+  for (var j = 1;j < (this.checkedDays.length - 1); j++) {
+      str += ' , ' + this.checkedDays[j];
   }
+  str += ' and ' + this.checkedDays[(this.checkedDays.length - 1)];
   return str;
 }
 
 window.onload = function() {
-  var days = document.getElementsByName('days');
-  var none = document.getElementById('none');
-  var checkbox = new CheckBox(days, none);
-  checkbox.bindEvents();
+  var daysElement = document.getElementsByName('days');
+  var noneCheckbox = document.getElementById('none');
+  var days = new Days(daysElement, noneCheckbox);
+  days.bindEvents();
 }
