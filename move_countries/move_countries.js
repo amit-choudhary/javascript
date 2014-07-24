b84453d50f6cef0d24334bb83cd7eda510b81955
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-24 14:50:11 +0530

    Move countries Exercise

---
+++ move_countries/move_countries.js
@@ -0,0 +1,39 @@
+function SelectCountries(firstList, secondList, addButton, removeButton) {
+this.firstList = firstList;
+this.secondList = secondList;
+this.addButton = addButton;
+this.removeButton = removeButton;
+this.selectedOption = '';
+}
+
+SelectCountries.prototype.bindEvents = function() {
+  var _this = this;
+  _this.addButton.addEventListener('click', function() {
+    _this.removeFromList(_this.firstList);
+    _this.addToList(_this.secondList);
+  } );
+  _this.removeButton.addEventListener('click', function() {
+    _this.removeFromList(_this.secondList);
+    _this.addToList(_this.firstList);
+  } );
+}
+
+SelectCountries.prototype.removeFromList = function(list) {
+  this.selectedOption = list.options[list.selectedIndex].text;
+  list.remove(list.selectedIndex);
+}
+
+SelectCountries.prototype.addToList = function(list) {
+  var option = document.createElement("option");
+  option.text = this.selectedOption;
+  list.add(option);
+}
+
+window.onload = function() {
+  var firstList = document.getElementById('first-select');
+  var secondList = document.getElementById('second-select');
+  var addButton = document.getElementById('add');
+  var removeButton = document.getElementById('remove');
+  var moveCountries = new SelectCountries(firstList, secondList, addButton, removeButton);
+  moveCountries.bindEvents();
+}
