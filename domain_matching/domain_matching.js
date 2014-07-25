79d2eeb287058bc1b6e592d71f51b1aae4024145
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-25 19:28:15 +0530

    Improve functionality, fix naming issues, fix closing tags issues and fix indentation

--- domain_matching/domain_matching.js
+++ domain_matching/domain_matching.js
@@ -1,14 +1,14 @@
-function Match(domain, form) {
-  this.domain = domain;
+function MatchDomain(domainBox, form) {
+  this.domainBox = domainBox;
   this.form = form;
 }
 
-Match.prototype.bindEvents = function() {
+MatchDomain.prototype.bindEvents = function() {
   var _this = this;
   _this.form.addEventListener('submit', function(event) {
-    var check = _this.checkFormat(_this.domain);
+    var check = _this.checkFormat(_this.domainBox);
     if (check) {
-      _this.extractDomain(_this.domain);
+      _this.extractDomain(_this.domainBox);
     }
     else {
       event.preventDefault();
@@ -16,8 +16,8 @@ Match.prototype.bindEvents = function() {
   } );
 }
 
-Match.prototype.checkFormat = function(textbox) {
-  var regex = /((http(s)?:\/\/)?(([a-zA-Z0-9])+(\.([a-zA-Z0-9]){2,3})+)+)/;
+MatchDomain.prototype.checkFormat = function(textbox) {
+  var regex = /^((http(s)?:\/\/)?(([a-zA-Z0-9])+(\.([a-zA-Z0-9]){2,3})+)+)$/;
   if (textbox.value == null || textbox.value == '') {
     alert('Url cant be blank');
     return false;
@@ -31,22 +31,37 @@ Match.prototype.checkFormat = function(textbox) {
   }
 }
 
-Match.prototype.extractDomain = function(textbox) {
-  var regex = /(\/(([a-zA-Z0-9])+(\.([a-zA-Z0-9]){2,3})+)+)(\/)?/;
+MatchDomain.prototype.extractDomain = function(textbox) {
+  var regex = /(\/?(([a-zA-Z0-9])+(\.([a-zA-Z0-9]){2,3})+)+)(\/?)?/;
       result = regex.exec(textbox.value);
-  result = result[0].split('/');
-  result = result[1].split('.');
+  if (result[0].indexOf('/') != -1) { 
+    result = result[0].split('/');
+    result = result[1].split('.');
+  }
+  else {
+    result = result[0].split('.');
+  }
   if (result.length == 2) {
     alert('Domain: ' + result[0] + '.com');
   }
-  else if (result.length == 3) {
-    alert('Domain: ' + result[0] + '  and Subdomain: ' + result[1] + '.com');
+  else if (result.length > 2) {
+    if (result[0] == "www") {
+      if (result.length == 3) {
+        alert('Domain: ' + result[1] + '.com');
+      }
+      if (result.length == 4) {
+        alert('Subdomain: ' + result[1] + '  and Domain: ' + result[2] + '.com');
+      }
+    }
+    else {
+      alert('Subdomain: ' + result[0] + '  and Domain: ' + result[1] + '.com');
+    }
   }
 }
 
 window.onload = function() {
-  var domain = document.getElementById('domain');
+  var domainBox = document.getElementById('domain-textbox');
       form = document.forms[0];
-      match = new Match(domain, form);
-  match.bindEvents();
+      matchdomain = new MatchDomain(domainBox, form);
+  matchdomain.bindEvents();
 }
