7d3b3066f7c1e54183385eda1b44fa5ee744ca99
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-28 15:09:44 +0530

    Improve functionality for empty url and refactor code

--- input_url/input_url.js
+++ input_url/input_url.js
@@ -2,8 +2,20 @@ function InputUrl() {
 }
 
 InputUrl.prototype.bindEvents = function() {
-  var url = prompt('Enter the url ');
-  window.open(url, "_blank", "width=400, height=450, menubar=no, scrollbars=no, titlebar=no");
+  var url = '';
+  while (!(url)) {
+    var url = prompt('Enter the url ').trim();
+    this.openUrl(url);
+  }
+}
+
+InputUrl.prototype.openUrl = function(url) {
+  if (url == null || url.length == 0) {
+      alert('Url cant be empty');
+    }
+    else {
+      window.open(url, "_blank", "width=400, height=450, menubar=no, scrollbars=no, titlebar=no");
+    }
 }
 
 window.onload = function() {
