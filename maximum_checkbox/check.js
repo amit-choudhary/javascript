a2ddb049651b8022f9472dc13c8db3cfcd63b443
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-28 15:50:25 +0530

    Optimise and refactor code, fix naming issues

--- maximum_checkbox/check.js
+++ maximum_checkbox/check.js
@@ -1,62 +1,52 @@
-function Days(daysElement, noneCheckbox) {
-  this.daysElement = daysElement;
+function DaysCountValidator(daysList, weekDays, noneCheckbox) {
+  this.daysList = daysList;
+  this.weekDays = weekDays;
   this.noneCheckbox = noneCheckbox;
-  this.count = 0;
   this.checkedDays = [];
 }
 
-Days.prototype.bindEvents = function() {
+DaysCountValidator.prototype.bindEvents = function() {
   var _this = this;
-  for(var i = 0; i < _this.daysElement.length; i++) {
-    _this.daysElement[i].addEventListener('click', function() {
-      _this.countCheck(this);
+    _this.daysList.addEventListener('click', function(event) {
+      _this.LimitCheckedDays(event.target);
       _this.noneCheckbox.checked = false;
     } );
-  }
   _this.noneCheckbox.addEventListener('click', function() {
     _this.mark(false);
-    _this.count = 0;
   } );
 }
 
-Days.prototype.countCheck = function(day) {
+DaysCountValidator.prototype.LimitCheckedDays = function(day) {
   if (day.checked == true) {
-    ++this.count;
     this.checkedDays.push(day.value);
   }
   else {
-    --this.count;
     var index = this.checkedDays.indexOf(day.value);
     this.checkedDays.splice(index, 1);
   }
-    if (this.count > 3) {
-      day.checked = false;
-      index = this.checkedDays.indexOf(day.value);
-      this.checkedDays.splice(index, 1);
-      --this.count;
-      alert('Only 3 days can be selected. You have already selected' + this.showSelected()); 
-    }
+  if (this.checkedDays.length > 3) {
+    day.checked = false;
+    index = this.checkedDays.indexOf(day.value);
+    this.checkedDays.splice(index, 1);
+    alert('Only 3 days can be selected. You have already selected' + this.showSelected()); 
+  }
 }
 
-Days.prototype.mark = function(flag) {
-  for (var i = 0; i < this.daysElement.length; i++) {
-    this.daysElement[i].checked = flag;
+DaysCountValidator.prototype.mark = function(flag) {
+  for (var i = 0; i < this.weekDays.length; i++) {
+    this.weekDays[i].checked = flag;
   }
   this.checkedDays = [];
 }
 
-Days.prototype.showSelected = function() {
-  var str = ' ' + this.checkedDays[0];
-  for (var j = 1;j < (this.checkedDays.length - 1); j++) {
-      str += ' , ' + this.checkedDays[j];
-  }
-  str += ' and ' + this.checkedDays[(this.checkedDays.length - 1)];
-  return str;
+DaysCountValidator.prototype.showSelected = function() {
+  return (' ' + this.checkedDays[0] + ', ' + this.checkedDays[1] + ' and ' + this.checkedDays[2]);
 }
 
 window.onload = function() {
-  var daysElement = document.getElementsByName('days');
-  var noneCheckbox = document.getElementById('none');
-  var days = new Days(daysElement, noneCheckbox);
-  days.bindEvents();
+  var daysList = document.getElementById('list'),
+      weekDays = document.getElementsByName('days'),
+      noneCheckbox = document.getElementById('none'),
+      daysValidator = new DaysCountValidator(daysList, weekDays, noneCheckbox);
+  daysValidator.bindEvents();
 }
