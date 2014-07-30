876d1f627cc34b890b5ee55da7b367275f47f335
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-30 13:13:04 +0530

    Optimise code

--- input_url/input_url.js
+++ input_url/input_url.js
@@ -1,24 +1,19 @@
 function InputUrl() {
 }
 
-InputUrl.prototype.bindEvents = function() {
+InputUrl.prototype.init = function() {
   var url = '';
   while (!(url)) {
     var url = prompt('Enter the url ').trim();
-    this.openUrl(url);
-  }
+    }
+  this.openUrl(url);
 }
 
 InputUrl.prototype.openUrl = function(url) {
-  if (url == null || url.length == 0) {
-    alert('Url cant be empty');
-  }
-  else {
-    window.open(url, "_blank", "width=400, height=450, menubar=no, scrollbars=no, titlebar=no");
-  }
+  window.open(url, "_blank", "width=400, height=450, menubar=no, scrollbars=no, titlebar=no");
 }
 
 window.onload = function() {
   var inputUrl = new InputUrl();
-  inputUrl.bindEvents();
+  inputUrl.init();
 }
