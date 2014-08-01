a50b0743ac3eae0698a9979859e1c6b751a4d8c9
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-01 15:20:36 +0530

    Optmise code and increse use of objects

--- form_validation/form.js
+++ form_validation/form.js
@@ -18,7 +18,7 @@ FormValidation.prototype.checkFormOnSubmit = function(event) {
 FormValidation.prototype.checkForEmpty = function(elementsArray) {
   var count = 0;
   for (var i = 0; i < elementsArray.length; i++) {
-    if (elementsArray[i].value == null || elementsArray[i].value == '') {
+    if (elementsArray[i].value.trim() == '') {
       var innertext = document.getElementsByClassName(elementsArray[i].id)[0].innerHTML;
       alert(innertext + ' cant be empty.');
     }
@@ -36,11 +36,10 @@ FormValidation.prototype.checkForEmpty = function(elementsArray) {
 
 FormValidation.prototype.checkTextArea = function() {
   var innertext = document.getElementsByClassName(this.textareaElement[0].id)[0].innerHTML;
-  var checktextareaboolean = this.checkForEmpty(this.textareaElement);
-  if (!checktextareaboolean) { 
+  if (!this.checkForEmpty(this.textareaElement)) { 
     return false;
   }
-  else if (this.textareaElement[0].value.length < 50) {
+  else if (this.textareaElement[0].value.trim().length < 50) {
     alert(innertext + 'cant be less than 50 characters.');
     return false;
   }
