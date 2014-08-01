71df6948b4dbd3e4bc07be231c2792207732e961
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-01 15:57:46 +0530

    Fix naming issues and optimise code

--- parent_children_checkbox/check.js
+++ parent_children_checkbox/check.js
@@ -1,37 +1,38 @@
-function Item(mainList, ItemList) {
-  this.mainList = mainList;
-  this.ItemList = ItemList;
+function Item(catalogue, itemList) {
+  this.catalogue = catalogue;
+  this.itemList = itemList;
 }
 
 Item.prototype.bindEvents = function() {
   var _this = this;
-  _this.mainList.addEventListener('click', function(event) {
+  _this.catalogue.addEventListener('click', function(event) {
   _this.performOnSubItems(event.target);
   } );
 }
 
 Item.prototype.performOnSubItems = function(list) {
   var subItemList = document.getElementsByName(list.className);
+  var subItem = document.getElementById(list.className);
   if (list.checked == true) {
     this.markSubItemList(subItemList, true);
-    document.getElementById(list.className).style.display = "block";
+    subItem.style.display = "block";
     list.scrollIntoView();
   }
   else {
     this.markSubItemList(subItemList, false);
-    document.getElementById(list.className).style.display = "none";
+    subItem.style.display = "none";
   }
 }
 
 Item.prototype.markSubItemList = function(subItemList, mark) {
-  for (var i = 0; i < subItemList.length ; i++) {
+  for (var i in subItemList) {
     subItemList[i].checked = mark;
   }
 }
 
 window.onload = function() {
-  var mainList = document.getElementById('main-list'),
+  var catalogue = document.getElementById('main-list'),
       ItemList = document.getElementsByName('parent-list'),
-      item = new Item(mainList, ItemList);
+      item = new Item(catalogue, ItemList);
   item.bindEvents();
 }
